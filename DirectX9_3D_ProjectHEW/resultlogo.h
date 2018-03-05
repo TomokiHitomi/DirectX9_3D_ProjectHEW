d2e67db107858a51570a71ba55b8eea4e6779601
yoshidaXX <kitihuton@gmail.com>
2018-03-05 10:41:06 +0900

    a

--- DirectX9_3D_ProjectHEW/resultlogo.h
+++ DirectX9_3D_ProjectHEW/resultlogo.h
@@ -1,13 +1,36 @@
 //=============================================================================
 //
 // リザルト画面処理 [resultlogo.h]
-// Author : 
+// Author : GP11B243 34 吉田太智
 //
 //=============================================================================
 #ifndef _RESULTLOGO_H_
 #define _RESULTLOGO_H_
 
 #include "main.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+
+
+#define	TEXTURE_RESULTLOGO			"data/TEXTURE/空.jpg"			// 読み込むテクスチャファイル名
+#define	TEXTURE_RESULTLOGO_LOGO	 	"data/TEXTURE/ガメオベラ.png"	// 読み込むテクスチャファイル名
+#define	TEXTURE_RESULTLOGO_LOGO2	"data/TEXTURE/ロゴ.png"		// 読み込むテクスチャファイル名
+
+#define	RESULTLOGO_LOGO_POS_X	(SCREEN_CENTER_X)		// リザルトロゴの位置(X座標)
+#define	RESULTLOGO_LOGO_POS_Y	(200)					// リザルトロゴの位置(Y座標)
+#define	RESULTLOGO_LOGO_WIDTH	(800 / 2)				// リザルトロゴの幅
+#define	RESULTLOGO_LOGO_HEIGHT	(240 / 2)				// リザルトロゴの高さ
+
+
+#define	RESULTLOGO_LOGO2_POS_X	(SCREEN_CENTER_X)		// リザルトロゴの位置(X座標)
+#define	RESULTLOGO_LOGO2_POS_Y	(500)					// リザルトロゴの位置(Y座標)
+#define	RESULTLOGO_LOGO2_WIDTH	(800 / 2)				// リザルトロゴの幅
+#define	RESULTLOGO_LOGO2_HEIGHT	(240 / 2)				// リザルトロゴの高さ
+
+#define	COUNT_APPERA_RESULTLOGO	(60)		// リザルトロゴ出現までの待ち時間	
+#define	LIMIT_COUNT_WAIT	(60 * 15)	// 待ち時間
 
 //*****************************************************************************
 // プロトタイプ宣言
